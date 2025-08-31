76ba84b6dcc23b20a57be7c19a0227d962765852
salsabeelnoor <salsabeelnoor@gmail.com>
2025-08-31 20:30:14 +0600

    http headers and cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-    return new Response('Profile API Data');
+import { type NextRequest  } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get('Authorization'));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"));
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "10");
+    console.log(cookieStore.get("resultsPerPage"));
+
+    return new Response('<h1>Profile API Data</h1>', {
+        headers: {
+            "Content-Type" : "text/html",
+            "Set-Cookie": "theme=dark",
+        }
+    });
 }
